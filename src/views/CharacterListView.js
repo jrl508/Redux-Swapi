1d1ce493a1299101b671407234bd384200ccaeee
jrl508 <jrl47@yahoo.com>
2019-06-19 19:08:38 -0400

    WIP map error in CharacterList.js

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,20 +2,27 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
+import { getCharacter } from '../actions'
+import Loader from 'react-loader-spinner'
+
 // import actions
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   componentDidMount() {
     // call our action
+    this.props.getCharacter();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      <div className="loading">
+        <Loader type="Oval" color="#00bfff" height="200" width="150" />
+      </div>
     }
     return (
       <div className="CharactersList_wrapper">
@@ -27,9 +34,15 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+const mapStateToProps = state => ({
+  fetching: state.fetching,
+  characters: state.characters
+})
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps /* mapStateToProps replaces null here */,
   {
     /* action creators go here */
+    getCharacter 
   }
 )(CharacterListView);
